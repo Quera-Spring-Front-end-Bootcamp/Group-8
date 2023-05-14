47c3592c72da2e43446be9c2ac665b2e6e9c3822
Arezoo Sadeghi <91677952+ArezooSDH@users.noreply.github.com>
2023-05-15 03:09:41 +0330

    base-project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import Forgotpassword from './pages/forgotpassword'
+import Register from './pages/register'
+import Login from './pages/login'
+
+
+function App() {
+
+  return (
+    <>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/forgotpassword">
+        <Forgotpassword />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ForgotPasswordForm/ForgotPasswordForm.jsx
@@ -0,0 +1,5 @@
+const ForgotPasswordForm = () => {
+    return <h1>Forget Password</h1>
+};
+
+export default ForgotPasswordForm;
