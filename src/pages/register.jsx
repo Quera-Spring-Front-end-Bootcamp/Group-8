1b45fabbcc077c1e617679f2dc338bf6a184b8ce
Arezoo Sadeghi <91677952+ArezooSDH@users.noreply.github.com>
2023-05-20 23:26:58 +0330

    register-page

---
+++ src/pages/register.jsx
@@ -0,0 +1,19 @@
+import { AuthFooter } from "../components/AuthFooter/AuthFooter";
+import { AuthHeader } from "../components/AuthHeader/AuthHeader";
+
+const register = () => {
+    return (
+        <>
+           <AuthHeader
+        text={"قبلاً ثبت نام کردی؟"}
+        buttonText={"ورود"}
+        link={"Login"}
+      />
+      <ForgotPassword />
+      <AuthFooter />
+        </>
+    )
+
+};
+
+export default register;
