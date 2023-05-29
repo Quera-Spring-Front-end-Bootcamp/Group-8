f3abce958c45afdfc771ecaae4c1160100ee2c0a
Sura Dyar <suradyarfromnowon@gmail.com>
2023-05-29 12:05:20 -0700

    button

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-const NewSpaceButton = (props) => {
-    return <div className={`cursor-pointer ${props.className}`} onClick={props.onClick}>{props.children}</div>
-};
 
-export default NewSpaceButton;
-=======
 const Button = (props) => {
   return (
     <div
