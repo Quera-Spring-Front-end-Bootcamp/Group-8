e666dbc1772b27ea4ddc97174ee6ebb8331c4139
Sura Dyar <suradyarfromnowon@gmail.com>
2023-05-29 12:12:34 -0700

    app-edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,21 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import ResetPassword from "./pages/resetpassword";
 import SendPasswordForm from "./pages/sendpassword";
-<<<<<<< HEAD
 import Layout from "./components/Dashboard/Layout/index";
 import Profile from './pages/profile'
 import ListView from './pages/listview'
 import ColumnView from './pages/columnview'
-=======
 
 import Layout from "./components/Dashboard/Layout/index";
 
->>>>>>> 02ffb7a45c32f4c1531fa73c8d98d1cdc57d0760
+
 import "./styles/App.css";
 
 function App() {
   return (
     <>
       <Route exact path="/" component={Login} />
-<<<<<<< HEAD
 
-=======
->>>>>>> 02ffb7a45c32f4c1531fa73c8d98d1cdc57d0760
       <Route path="/login">
         <Login />
       </Route>
@@ -42,7 +37,7 @@ function App() {
       <Route path="/layout">
         <Layout />
       </Route>
-<<<<<<< HEAD
+
       <Route path="/profile">
         <Profile />
       </Route>
@@ -53,8 +48,6 @@ function App() {
           <ColumnView/>
       </Route>
       
-=======
->>>>>>> 02ffb7a45c32f4c1531fa73c8d98d1cdc57d0760
     </>
   );
 }
