065597de9c3d34a6aac4536700dd1b205d736f51
Hussein Shirvani <husseinshirvani@gmail.com>
2023-05-29 20:41:14 +0330

    Update Button.jsx

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,5 +1,17 @@
-const NewSpaceButtom = (props) => {
-    return <div className={`cursor-pointer ${props.className}`} onClick={props.onClick}>{props.children}</div>
+const Button = (props) => {
+  return (
+    <div
+      className={`cursor-pointer w-max flex items-center justify-center gap-[8px] rounded-[8px] ${props.className}`}
+      onClick={props.onClick}
+    >
+      {props.icon && (
+        <span className="material-symbols-rounded">
+          {props.icon}
+        </span>
+      )}
+      {props.children}
+    </div>
+  );
 };
 
-export default NewSpaceButtom;
+export default Button;
