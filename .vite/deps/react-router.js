411dfbe8c50d308dd77471fd6dce51e0139c1409
Arezoo Sadeghi <arezoo.sdh@gmail.com>
2023-07-03 14:27:23 +0330

    packages

---
+++ .vite/deps/react-router.js
@@ -0,0 +1,41 @@
+import {
+  MemoryRouter,
+  Prompt,
+  Redirect,
+  Route,
+  Router,
+  StaticRouter,
+  Switch,
+  context,
+  generatePath,
+  historyContext,
+  matchPath,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+  withRouter
+} from "./chunk-Y3TZ5MKA.js";
+import "./chunk-IY23UCNA.js";
+import "./chunk-KM5JISHU.js";
+import "./chunk-ST3U5LCA.js";
+import "./chunk-DFKQJ226.js";
+export {
+  MemoryRouter,
+  Prompt,
+  Redirect,
+  Route,
+  Router,
+  StaticRouter,
+  Switch,
+  historyContext as __HistoryContext,
+  context as __RouterContext,
+  generatePath,
+  matchPath,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+  withRouter
+};
+//# sourceMappingURL=react-router.js.map
