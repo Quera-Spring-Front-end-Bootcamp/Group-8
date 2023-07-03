9e3467bf764912a01ec86c51f4ac0150ac9a0e90
Arezoo Sadeghi <arezoo.sdh@gmail.com>
2023-07-03 16:52:36 +0330

    Revert "packages"
    This reverts commit b82f5c4412d6ae5166f3c4715549d1eb4992a3c1.

--- .vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-NREMUTAN.js";
-import "./chunk-ST3U5LCA.js";
-import "./chunk-DFKQJ226.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
